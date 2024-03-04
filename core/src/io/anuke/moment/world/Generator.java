5dea67956833b7f73d7d27c5d7ec238e99311cc4
camelStyleUser <124447863+camelStyleUser@users.noreply.github.com>
2024-03-04 18:51:51 +0200

    epic coding

--- core/src/io/anuke/moment/world/Generator.java
+++ core/src/io/anuke/moment/world/Generator.java
@@ -9,10 +9,12 @@ import io.anuke.moment.Moment;
 import io.anuke.ucore.noise.Noise;
 
 public class Generator{
-	static final int black = Color.rgba8888(Color.BLACK);
-	static final int white = Color.rgba8888(Color.WHITE);
-	static final int red = Color.rgba8888(Color.RED);
-	static final int blue = Color.rgba8888(Color.BLUE);
+	static final int black = Color.rgba8888(Color.BLACK);//000000ff
+	static final int white = Color.rgba8888(Color.WHITE);//ffffffff
+	static final int red = Color.rgba8888(Color.RED);//ff0000ff
+	static final int blue = Color.rgba8888(Color.BLUE);//0000ffff
+	static final int gray = Color.rgba8888(Color.LIGHT_GRAY);//bfbfbfff
+	static final int orange = Color.rgba8888(Color.ORANGE);//ffa500ff
 	public static void generate(Tile[][] tiles, String mapname){
 	generate(tiles,mapname,true);
 	}
@@ -24,6 +26,14 @@ public class Generator{
 		pix = new Pixmap(Gdx.files.absolute(mapname));
 		}
 		Noise.setSeed(MathUtils.random(0, 99999));
+		boolean hasore=false;
+		for(int x = 0; x < tiles.length; x ++){
+			for(int y = 0; y < tiles.length; y ++){
+			int color = pix.getPixel(x, pix.getHeight()-1-y);
+			if(color==gray||color==orange){hasore=true;break;}
+			}
+			if(hasore)break;
+		}
 		for(int x = 0; x < tiles.length; x ++){
 			for(int y = 0; y < tiles.length; y ++){
 				TileType floor = TileType.stone;
@@ -31,11 +41,11 @@ public class Generator{
 				
 				int color = pix.getPixel(x, pix.getHeight()-1-y);
 				
-				if(Noise.nnoise(x, y, 8, 1) > 0.22){
+				if((!hasore)&&Noise.nnoise(x, y, 8, 1) > 0.22){
 					floor = TileType.iron;
 				}
 				
-				if(Noise.nnoise(x, y, 6, 1) > 0.245){
+				if((!hasore)&&Noise.nnoise(x, y, 6, 1) > 0.245){
 					floor = TileType.coal;
 				}
 				
@@ -47,6 +57,10 @@ public class Generator{
 					Moment.i.core = tiles[x][y];
 				}else if(color == red){
 					Moment.i.spawnpoints.add(tiles[x][y]);
+				}else if(color==gray){
+				floor = TileType.coal;
+				}else if(color==orange){
+				floor=TileType.iron;
 				}
 				
 				tiles[x][y].setBlock(block);
